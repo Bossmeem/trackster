7106cd7905f011389aab60ad3fc0ed6b29df4f58
BSSMM <williamrloyd@gmail.com>
2017-11-15 16:11:12 -0500

    js ajax request

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,15 @@
-var Trackster = {};
+$(document).ready(function() {
+  var Trackster = {};
+  $('#search').click(function() {
+    Trackster.searchTracksByTitle($('#input1').val()
+    );
+  });
+  const API_KEY = '46afba08ac51b2749efffb5f17236e03';
+
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -13,5 +20,15 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + 'title' + '&api_key=' + API_KEY + '&format=json';
+    datatype: jsonp;
+    success: {
+      funtion(){
+        console.log('results')
+      }
+    }
+  });
 };
+
+});
